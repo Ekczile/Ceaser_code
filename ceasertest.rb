df61d22e90186ab90f21e909662100f6bca7cbc4
Steven <ekczile@gmail.com>
2018-10-31 21:48:30 -0400

    space between words fail

--- ceasertest.rb
+++ ceasertest.rb
@@ -8,4 +8,10 @@ class Test_for_Ceasercode < Minitest::Test
     def test_for_encrypt
         assert_equal("f", tossed_salad("a"))
     end
+    def test_for_words
+        assert_equal("mjqu", tossed_salad("help"))
+    end
+    def test_for_two_words
+        assert_equal("", tossed_salad("send backup"))
+    end
 end
