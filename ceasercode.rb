2466f6d062374545eebf3ffb3b87024fb9d69325
Steven <ekczile@gmail.com>
2018-11-05 11:30:28 -0500

    now accounts for integers and upcase

--- ceasercode.rb
+++ ceasercode.rb
@@ -1,11 +1,14 @@
 def tossed_salad(strings_to_be_encoded)
-# array of letters
+# array of letters`123rf
 letters = ("a".."z").to_a
+upletters = ("A".."Z").to_a
+numbers = ("0".."9").to_a
 # make variable that adds letters as key then make letter rotated by 5 the value
-encrypt = letters.zip(letters.rotate(5)).to_h
+encrypt = letters.zip(letters.rotate(5)).to_h.merge!(upletters.zip(upletters.rotate(5)).to_h).merge!(numbers.zip(numbers.rotate(5)).to_h)
 # merge new keys and value after the rotate so they stay at normal index's
 encrypt.merge!(" " => " "); encrypt.merge!("," => ","); encrypt.merge!("?" => "?"); encrypt.merge!("!" => "!"); encrypt.merge!("\"" => "\""); encrypt.merge!("." => "."); encrypt.merge!("-" => "-");
 # conditional for if any of keys of the array match any of my characters in strings_to_be_encoded if they dont match put didn't work
+p (encrypt.keys & strings_to_be_encoded.split(""))
     if (encrypt.keys & strings_to_be_encoded.split("")).empty?
         p "didn't work"
     else
@@ -17,7 +20,9 @@ end
 
 def untossed_salad(strings_to_be_encoded)
     letters = ("a".."z").to_a
-    encrypt = letters.zip(letters.rotate(-5)).to_h
+    upletters = ("A".."Z").to_a
+    numbers = ("0".."9").to_a
+    encrypt = letters.zip(letters.rotate(5)).to_h.merge!(upletters.zip(upletters.rotate(5)).to_h).merge!(numbers.zip(numbers.rotate(5)).to_h)
     encrypt.merge!(" " => " "); encrypt.merge!("," => ","); encrypt.merge!("?" => "?"); encrypt.merge!("!" => "!"); encrypt.merge!("\"" => "\""); encrypt.merge!("." => "."); encrypt.merge!("-" => "-");
         if (encrypt.keys & strings_to_be_encoded.split("")).empty?
         p "didn't work"
--- ceasertest.rb
+++ ceasertest.rb
@@ -24,15 +24,33 @@ class Test_for_Ceasercode < Minitest::Test
         assert_equal("didn't work", tossed_salad("\@##$%#%^$%*(_+="))
     end
     def test_one_untossed
-        assert_equal("a", untossed_salad("f"))
+        assert_equal("f", untossed_salad("a"))
     end
     def test_for_words_unencrypted
-        assert_equal("help", untossed_salad("mjqu"))
+        assert_equal("mjqu", untossed_salad("help"))
     end
     def test_for_two_words
-        assert_equal("nziy wvxfpk kgzvnz!", untossed_salad("send backup please!"))
+        assert_equal("send backup please!", untossed_salad("nziy wvxfpk kgzvnz!"))
     end
     def test_for_long_message
-        assert_equal("voovxf vo admno npigdbco!", untossed_salad("attack at first sunlight!"))
+        assert_equal("attack at first sunlight!", untossed_salad("voovxf vo admno npigdbco!"))
+    end
+    def test_for_uppercase
+        assert_equal("MJQQT", tossed_salad("HELLO"))
+    end
+    def test_for_both_up_and_down_case
+        assert_equal("YmNx Nx GtYm!,",tossed_salad("ThIs Is BoTh!,"))
+    end
+    def test_for_numbers
+        assert_equal("6,7,8,9,0",tossed_salad("1,2,3,4,5"))
+    end
+    def test_ammount_of_solderies
+        assert_equal("xjsi 65,5555 xtqinjwx",tossed_salad("send 10,0000 soldiers"))
+    end
+    def test_ammount_of_soldiers_decoded
+        assert_equal("send 10,0000 soldiers",untossed_salad("nziy 65,5555 njgydzmn"))
+    end
+    def test_for_up_soldiers_decode
+        assert_equal("", untossed_salad("NziY 65,5555 NjgydzMn"))
     end
 end
